137a61d90378b4ce5d8d1ab4573c76158917ac03
johnrenko <vishnou@gmail.com>
2015-10-11 16:37:02 +1100

    Connected to Parse.com

---
+++ app/src/main/java/jd/secrets/SecretsApplication.java
@@ -0,0 +1,21 @@
+package jd.secrets;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class SecretsApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "d3a2Z4QuMss5YZeYfR6IqAUB5dPmphNXQ6JUfezk", "qXl5ZYLwehBW8AqWqtFZu2QhguxNNdstkzERyhzL");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+
+}
