0f406c36b79a4554cf4fc4fd31cbdbd428b7eecd
johnrenko <vishnou@gmail.com>
2015-10-11 17:50:32 +1100

    Adding error dialog

--- app/src/main/java/jd/secrets/SignUpActivity.java
+++ app/src/main/java/jd/secrets/SignUpActivity.java
@@ -1,19 +1,49 @@
 package jd.secrets;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    protected EditText mEmail;
+    protected EditText mPassword;
+    protected Button mSignupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mEmail = (EditText)findViewById(R.id.userNameSignUpField);
+        mPassword = (EditText)findViewById(R.id.passwordSignUpField);
+        mSignupButton = (Button)findViewById(R.id.signUpButton);
+
+        mSignupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                email = email.trim();
+                password = password.trim();
+
+                if (email.isEmpty() || password.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.signUpError)
+                        .setTitle(R.string.signUpErrorTitle)
+                        .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
 
     }
 
